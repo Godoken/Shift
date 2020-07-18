cad1f5379964bca020124db28594151fbd05a1f0
Mikhail Plekhanov <mixrpg@mail.ru>
2020-07-18 09:25:37 +0700

    Some Interface

--- app/src/main/java/com/example/shift/MainActivity.kt
+++ app/src/main/java/com/example/shift/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.shift
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            if (imageView.isVisible) {
+                imageView.visibility = GONE
+            } else {
+                imageView.visibility = VISIBLE
+            }
+        }
     }
 }
